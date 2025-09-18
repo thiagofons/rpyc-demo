292d1821b0ce8d45c570c280f8a8ed1d279aa6fe
Thiago Arruda <thiago_arruda@id.uff.br>
2025-09-18 10:56:10 -0300

    📦 NEW: Questão 6

--- client/client.py
+++ client/client.py
@@ -3,24 +3,21 @@ import sys
 import os
 import time
 
-
-
-
 if len(sys.argv) < 2:
   exit("Usage {} SERVER".format(sys.argv[0]))
 
 server = sys.argv[1]
 
-conn = rpyc.connect(server,18861)
+timeout_seconds = 60
+conn = rpyc.connect(server, 18861, config={"sync_request_timeout": timeout_seconds})
 
 n = 10000
 
-vector = [i for i in range(0, n)]
+vector: list[int] = [i for i in range(0, n)]
 
-for i in range(10):
-  start = time.time()
-  sum = conn.root.sum_vector(vector)
-  end = time.time()
+start = time.time()
+sum = conn.root.exposed_sum_vector(vector)  
+end = time.time()
 
-  elapsed_time = end - start
-  print(f"{elapsed_time:.2f}")
+elapsed_time = end - start
+print(f"{elapsed_time:.2f}")
